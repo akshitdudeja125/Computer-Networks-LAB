e3e230e71a4f772b024b464452cef46f467c8a6a
Ishan-A-Mardani <mardaniishan@gmail.com>
2024-03-04 11:08:14 +0530

    q1

--- Assignment3/q1.c
+++ Assignment3/q1.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
 #include <arpa/inet.h>
 #include <ctype.h>
 #include <dirent.h>
@@ -16,160 +18,123 @@
 #include <unistd.h>
 
 #define PORT 3003
-#define BUFFER_SIZE 104857600
+#define BUFFER_SIZE 1e9
+#define IP "127.0.0.1"
 
-const char *get_file_extension(const char *file_name)
+const char *extractFileExtension(const char *file_name)
 {
     const char *dot = strrchr(file_name, '.');
     if (!dot || dot == file_name)
-    {
         return "";
-    }
     return dot + 1;
 }
 
-const char *get_mime_type(const char *file_ext)
+const char *generateMimeType(const char *file_ext)
 {
     if (strcasecmp(file_ext, "html") == 0 || strcasecmp(file_ext, "htm") == 0)
-    {
         return "text/html";
-    }
     else if (strcasecmp(file_ext, "txt") == 0)
-    {
         return "text/plain";
-    }
     else if (strcasecmp(file_ext, "jpg") == 0 || strcasecmp(file_ext, "jpeg") == 0)
-    {
         return "image/jpeg";
-    }
     else if (strcasecmp(file_ext, "png") == 0)
-    {
         return "image/png";
-    }
     else
-    {
         return "application/octet-stream";
-    }
 }
 
-char *url_decode(const char *src)
+char *decodeURL(const char *src)
 {
     size_t src_len = strlen(src);
-    char *decoded = malloc(src_len + 1);
+    char *decoded = (char *)malloc(src_len + 1);
     size_t decoded_len = 0;
 
-    // decode %2x to hex
     for (size_t i = 0; i < src_len; i++)
     {
         if (src[i] == '%' && i + 2 < src_len)
         {
-            int hex_val;
-            sscanf(src + i + 1, "%2x", &hex_val);
-            // printf("%c %c %d\n", hex_val);
-            decoded[decoded_len++] = hex_val;
+            char hex_val_str[3] = {src[i + 1], src[i + 2], '\0'};
+            int hex_val = (int)strtol(hex_val_str, NULL, 16);
+            decoded[decoded_len++] = (char)hex_val;
             i += 2;
         }
         else
-        {
             decoded[decoded_len++] = src[i];
-        }
     }
 
-    // add null terminator
     decoded[decoded_len] = '\0';
     return decoded;
 }
 
-void build_http_response(const char *file_name,
-                         const char *file_ext,
-                         char *response,
-                         size_t *response_len)
+void generateResponse(const char *file_name, const char *file_ext, char *response, size_t *response_len)
 {
-    // build HTTP header
-    const char *mime_type = get_mime_type(file_ext);
+    const char *mime_type = generateMimeType(file_ext);
     char *header = (char *)malloc(BUFFER_SIZE * sizeof(char));
-    snprintf(header, BUFFER_SIZE,
-             "HTTP/1.1 200 OK\r\n"
-             "Content-Type: %s\r\n"
-             "\r\n",
+    snprintf(header, BUFFER_SIZE, "HTTP/1.1 200 OK\r\n"
+                                  "Content-Type: %s\r\n\r\n",
              mime_type);
 
-    // if file not exist, response is 404 Not Found
     int file_fd = open(file_name, O_RDONLY);
     if (file_fd == -1)
     {
-        snprintf(response, BUFFER_SIZE,
-                 "HTTP/1.1 404 Not Found\r\n"
-                 "Content-Type: text/plain\r\n"
-                 "\r\n"
-                 "404 Not Found");
+        snprintf(response, BUFFER_SIZE, "HTTP/1.1 404 Not Found\r\n"
+                                        "Content-Type: text/plain\r\n\r\n"
+                                        "404 Not Found");
         *response_len = strlen(response);
         return;
     }
 
-    // get file size for Content-Length
     struct stat file_stat;
     fstat(file_fd, &file_stat);
     off_t file_size = file_stat.st_size;
 
-    // copy header to response buffer
     *response_len = 0;
     memcpy(response, header, strlen(header));
     *response_len += strlen(header);
 
-    // copy file to response buffer
     ssize_t bytes_read;
-    while ((bytes_read = read(file_fd,
-                              response + *response_len,
-                              BUFFER_SIZE - *response_len)) > 0)
-    {
+    while ((bytes_read = read(file_fd, response + *response_len, BUFFER_SIZE - *response_len)) > 0)
         *response_len += bytes_read;
-    }
+
     free(header);
     close(file_fd);
 }
 
 void *handle_client(void *arg)
 {
-    int client_fd = *((int *)arg);
+    int client_socket = *((int *)arg);
     char *buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
 
-    // receive request data from client and store into buffer
-    ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE, 0);
-    printf("client_fd: %d\n", client_fd);
+    ssize_t bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+    printf("client_socket: %d\n", client_socket);
     printf("Buffer: %s\n", buffer);
     if (bytes_received > 0)
     {
-        // find "GET /" and " HTTP/1" strings to extract filename
         char *get_ptr = strstr(buffer, "GET /");
         if (get_ptr != NULL)
         {
             char *http_ptr = strstr(get_ptr, " HTTP/1");
             if (http_ptr != NULL)
             {
-                *http_ptr = '\0'; // null-terminate the filename
+                *http_ptr = '\0';
 
-                // decode URL and extract filename
-                char *file_name = url_decode(get_ptr + 5); // Skip "GET /"
+                char *file_name = decodeURL(get_ptr + 5);
 
-                // get file extension
                 char file_ext[32];
-                strcpy(file_ext, get_file_extension(file_name));
+                strcpy(file_ext, extractFileExtension(file_name));
 
-                // build HTTP response
                 char *response = (char *)malloc(BUFFER_SIZE * 2 * sizeof(char));
                 size_t response_len;
-                build_http_response(file_name, file_ext, response, &response_len);
+                generateResponse(file_name, file_ext, response, &response_len);
 
-                // send HTTP response to client
-                send(client_fd, response, response_len, 0);
+                send(client_socket, response, response_len, 0);
 
                 free(response);
                 free(file_name);
             }
         }
     }
-    close(client_fd);
+    close(client_socket);
     free(arg);
     free(buffer);
     return NULL;
@@ -177,23 +142,25 @@ void *handle_client(void *arg)
 
 int main(int argc, char *argv[])
 {
-    int server_fd;
+    int server_socket;
     struct sockaddr_in server_addr;
 
-    // create server socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // config socket
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    // server_addr.sin_addr.s_addr = INADDR_ANY;
+    if (inet_pton(AF_INET, IP, &(server_addr.sin_addr)) <= 0)
+    {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
     server_addr.sin_port = htons(PORT);
 
-    // bind socket to port
-    if (bind(server_fd,
+    if (bind(server_socket,
              (struct sockaddr *)&server_addr,
              sizeof(server_addr)) < 0)
     {
@@ -201,36 +168,33 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    // listen for connections
-    if (listen(server_fd, 10) < 0)
+    if (listen(server_socket, 10) < 0)
     {
         perror("listen failed");
         exit(EXIT_FAILURE);
     }
 
-    printf("Server listening on port %d\n", PORT);
+    printf("Server listening on Port %d\n", PORT);
     while (1)
     {
-        // client info
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
-        int *client_fd = malloc(sizeof(int));
+        int *client_socket = malloc(sizeof(int));
 
-        // accept client connection
-        if ((*client_fd = accept(server_fd,
-                                 (struct sockaddr *)&client_addr,
-                                 &client_addr_len)) < 0)
+        if ((*client_socket = accept(server_socket,
+                                     (struct sockaddr *)&client_addr,
+                                     &client_addr_len)) < 0)
         {
-            perror("accept failed");
+            perror("Accept Failed");
             continue;
         }
 
         // create a new thread to handle client request
         pthread_t thread_id;
-        pthread_create(&thread_id, NULL, handle_client, (void *)client_fd);
+        pthread_create(&thread_id, NULL, handle_client, (void *)client_socket);
         pthread_detach(thread_id);
     }
 
-    close(server_fd);
+    close(server_socket);
     return 0;
 }
